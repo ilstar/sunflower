fdded8e2f6cc9fdeca21d4b6e505cc6496024662
Fred Liang <fred@instacart.com>
2017-02-18 23:08:00 -0800

    Add icon

--- main.js
+++ main.js
@@ -32,6 +32,9 @@ function createWindow () {
   })
 }
 
+app.dock.setIcon("./sunflower.png")
+app.setName("Sunflower")
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
